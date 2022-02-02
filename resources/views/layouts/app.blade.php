74bf8ed1d8b0d58aedfbfbd4e1df104e48b83ba2
andreafontaneto <andrea.fontaneto@gmail.com>
2022-02-02 17:56:57 +0100

    get posts + print posts list + make link in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,7 @@
                 <a class="navbar-brand" href="{{ route('home') }}">
                     Vai al Sito
                 </a>
+                <a href="{{ route('admin.posts.index') }}">Elenco post</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
